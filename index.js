a04f3a759138a8e5f49a12130c9bb64fbfa8071f
vbw47 <129989595+vbw47@users.noreply.github.com>
2023-04-05 20:38:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://wancho-bread-olly-default-rtdb.firebaseio.com/"
+    databaseURL: "https://wancho-bread-olly-dos-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings)
